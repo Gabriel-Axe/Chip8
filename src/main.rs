5a3176fba0d3e184923b1ff6e2a87f7c08b67059
gabriel <gabrielaxesm@gmail.com>
2026-06-24 18:55:23 -0300

    feat: read rom from adjacent roms directory

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::{env, fs};
+use std::{env, fs, path::PathBuf};
 
 fn main() {
-    let data = fs::read("src/hi.md").unwrap();
-    println!("{}", String::from_utf8(data).unwrap());
-    // for smt in data {
-        // println!("{}", String::from_utf8(smt);
-    // }
-    // let cur_dir = env::current_dir().unwrap();
-    // println!("{}", cur_dir.display());
-    // let data: Vec<u8> = fs::read(path)
+    let blitz = load_rom("Blitz [David Winter].ch8");
+    println!("Loaded Blitz: {} bytes", blitz.len());
+}
+
+fn load_rom(filename: &str) -> Vec<u8> {
+    let mut path = PathBuf::from("roms/chip8-roms/games");
+    path.push(filename);
+
+    fs::read(&path)
+        .unwrap_or_else(|err| panic!("Could not load ROM: {}, reason: {}", path.display(), err))
 }
