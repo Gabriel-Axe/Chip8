1adefbdc5c8218a0c47abdfc866012642a235125
gabriel <gabrielaxesm@gmail.com>
2026-06-24 18:32:57 -0300

    feat: learn to read bytes from files

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,12 @@
+use std::{env, fs};
+
 fn main() {
+    let data = fs::read("src/hi.md").unwrap();
+    println!("{}", String::from_utf8(data).unwrap());
+    // for smt in data {
+        // println!("{}", String::from_utf8(smt);
+    // }
+    // let cur_dir = env::current_dir().unwrap();
+    // println!("{}", cur_dir.display());
+    // let data: Vec<u8> = fs::read(path)
 }
