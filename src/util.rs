5ef0948eed393f862c7b45c57ea9680c3f75cb33
Gabriel <gabrielaxesm@gmail.com>
2026-08-01 19:45:50 -0300

    refactor: rename util functions

--- src/util.rs
+++ src/util.rs
@@ -1,20 +1,14 @@
-pub fn u16_to_decimal(numbers: Vec<u16>) {
-    for num in numbers {
-        println!("{:016b}", num);
-    }
+pub fn print_decimal_in_16_bits_binary(number: &u8) {
+    println!("{:16b}", number);
 }
 
-pub fn decimal_to_binary_one_number(number: &u8) {
-    println!("{:08b}", number);
-}
-
-pub fn decimal_to_binary_only(numbers: &[u8]) {
+pub fn print_bytes_in_binary(numbers: &[u8]) {
     for &num in numbers {
         println!("{:08b}", num);
     }
 }
 
-pub fn read_rom_as_hex(rom: &Vec<u8>) {
+pub fn print_bytes_in_hex(rom: &Vec<u8>) {
     for (i, byte) in rom.iter().enumerate() {
         print!("{:02X} ", byte);
         if (i + 1) % 16 == 0 { println!(""); }
@@ -23,20 +17,13 @@ pub fn read_rom_as_hex(rom: &Vec<u8>) {
     println!("");
 }
 
-pub fn read_byte_by_byte_from_file(bytes: &Vec<u8>) {
+pub fn print_bytes_vector(bytes: &Vec<u8>) {
     for byte in bytes {
         println!("{}", byte);
     }
 }
 
-// pub fn convert_8bits_to_16bits(bytes: &Vec<u8>) -> Vec<u16> {
 pub fn convert_u8_to_u16(bytes: &[u8]) -> Result<Vec<u16>, String> {
-    // if bytes.len() % 2 != 0 {
-    //     let res = String::from_str("Cannot convert u8 vector, there is a odd number of values");
-    //     let mes = res.expect("Could not unwrap :(");
-    //     return Err(mes);
-    // }
-
     let words: Vec<u16> = bytes
         .chunks_exact(2)
         .map(|chunk| u16::from_le_bytes([chunk[0], chunk[1]]))
