eee4e82d307a2d96ee04c7e496cf22d7ab164cb2
Gabriel <gabrielaxesm@gmail.com>
2026-08-02 17:37:21 -0300

    feat: attempt miserably to create a window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{fs};
 
+use winit::{event_loop::EventLoop, window::WindowAttributes};
+
 use crate::{chip8::Chip8, util::{convert_bytes_u8_to_u16, get_instruction_nibble, print_bytes_in_16_binary, print_bytes_in_binary}};
 
 mod chip8;
@@ -9,9 +11,14 @@ mod register;
 mod util;
 
 fn main() {
+    let event_loop = EventLoop::new().expect("Could not create winit event loop");
+    let window = event_loop.create_window(WindowAttributes::new()).expect("Could not create window");
+}
+
+fn emulator() {
     let blitz = load_rom("Blitz [David Winter].ch8");
     let chip8 = Chip8::new();
-    chip8.interpret(blitz);
+    chip8.interpret(&blitz);
 }
 
 fn load_rom(filename: &str) -> Vec<u16> {
