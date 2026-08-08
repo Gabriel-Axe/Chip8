e483edf721e3f66d7da0b46847fd9d3638f74467
Gabriel <gabrielaxesm@gmail.com>
2026-08-08 17:32:17 -0300

    feat: I HATE PROGRAMING and btw managed to create windoAIHATEPROGRAMING

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,12 @@ mod util;
 //     pixels: Option<Pixels<'static>>,
 // }
 
-const WIDTH: usize = 64;
-const HEIGHT: usize = 32;
+const WIDTH: usize = 30;
+const HEIGHT: usize = 30;
 const SCALE: usize = 10;
+// const WIDTH: usize = 64;
+// const HEIGHT: usize = 32;
+// const SCALE: usize = 10;
 
 // impl Default for App {
 //     fn default() -> Self {
@@ -109,8 +112,11 @@ const SCALE: usize = 10;
 //     }
 // }
 
+fn rgb(r: u8, g:u8, b:u8) -> u32 {
+    ((r as u32) << 16) | ((g as u32) << 8) | (b as u32)
+}
+
 fn main() {
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
     let mut window = Window::new(
         "Test - ESC to exit",
@@ -119,22 +125,32 @@ fn main() {
         WindowOptions::default(),
     )
     .unwrap_or_else(|e| {
-        panic!("{}", e);
+        panic!("AAAAAAAAAAAAAAAAAAAAAAAAAA {}", e);
     });
 
-    // Limit to max ~60 fps update rate
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
     window.set_target_fps(60);
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        for i in buffer.iter_mut() {
-            *i = 0; // write something more funny here!
+        buffer.fill(rgb(0, 0, 0));
+
+        for x in 0..WIDTH {
+            for y in 0..WIDTH {
+                if x == 0 || x == WIDTH - 1 || y == 0 || y == HEIGHT - 1 {
+                    buffer[y * WIDTH + x] = rgb(255, 255, 255);
+                }
+            }
         }
 
-        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        let center_x = WIDTH / 2;
+        let center_y = HEIGHT / 2;
+        buffer[center_y * WIDTH + center_x ] = rgb(255, 0, 0);
+
         window
-            .update_with_buffer(&buffer, WIDTH, HEIGHT)
-            .unwrap();
-        }   
+            .update_with_buffer(&buffer, WIDTH , HEIGHT )
+            .expect("Failed to update winodw");
+
+        }
 }
 
 fn emulator() {
